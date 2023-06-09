a3325e819f9ba70e513aae2d87de97ca991fd4bb
Victor Gorka De Prada Arnedo <vde-prad@student.42malaga.com>
2023-06-09 13:16:03 +0200

    grey image displayed on a window

--- includes/cub3D.h
+++ includes/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: vde-prad <vde-prad@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 17:08:08 by fmanzana          #+#    #+#             */
-/*   Updated: 2023/06/01 12:58:06 by vde-prad         ###   ########.fr       */
+/*   Updated: 2023/06/08 15:46:00 by vde-prad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,15 @@
 
 //*********************** STANDARD LIBRARIES *********************************//
 # include "./libft_plus/libft.h"
+# include "../MLX42/include/MLX42/MLX42.h"
 
 //************************** DEFINITIONS *************************************//
+# define WIDTH 1024
+# define HEIGHT 512
+typedef struct s_rc{
+	mlx_t		*mlx;
+	mlx_image_t	*img;
+}	t_rc;
 
 //*************************** FUNCTIONS **************************************//
 
--- src/raycaster.c
+++ src/raycaster.c
@@ -1,6 +1,26 @@
 #include "../includes/cub3D.h"
 
-int	main(void)
+void	ft_key_hook(mlx_key_data_t keydata, void *param)
 {
-	printf("Hola Mundo\n");
+	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_RELEASE)
+	{
+		mlx_terminate(((t_rc *)param)->mlx);
+		exit(0);
+	}	
+	(void)param;
+}
+
+int32_t	main(void)
+{
+	t_rc		data;
+
+	data.mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true);
+	if (!data.mlx)
+		return (EXIT_FAILURE);
+	data.img = mlx_new_image(data.mlx, WIDTH, HEIGHT);
+	mlx_image_to_window(data.mlx, data.img, 0, 0);
+	mlx_key_hook(data.mlx, &ft_key_hook, &data);
+	mlx_loop(data.mlx);
+	mlx_terminate(data.mlx);
+	return (EXIT_SUCCESS);
 }
