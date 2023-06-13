e948c51801c54fe265253d7b966140d7b6dd8717
Victor Gorka De Prada Arnedo <vde-prad@student.42malaga.com>
2023-06-13 18:19:11 +0200

    minimap without collisions and raycasting finished

--- includes/cub3D.h
+++ includes/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: vde-prad <vde-prad@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 17:08:08 by fmanzana          #+#    #+#             */
-/*   Updated: 2023/06/09 17:59:55 by vde-prad         ###   ########.fr       */
+/*   Updated: 2023/06/13 18:16:03 by vde-prad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,19 @@
 # include "../MLX42/include/MLX42/MLX42.h"
 
 //************************** DEFINITIONS *************************************//
-# define WIDTH 1024
-# define HEIGHT 512
+# define WIDTH		1024
+# define HEIGHT		512
+# define P_SIZE		8
+# define MAP_WIDTH	1024/4
+# define MAP_HEIGHT	512/2
+# define MAP_X		8
+# define MAP_Y		8
+
 typedef struct s_rc{
 	mlx_t		*mlx;
-	mlx_image_t	*img;
+	mlx_image_t	*img_p;
+	mlx_image_t	*img_map;
+	int			map_size;
 }	t_rc;
 
 //*************************** FUNCTIONS **************************************//
