50f14bfe5cd9265fd54ca28dce0ead15052e086d
Victor Gorka De Prada Arnedo <vde-prad@student.42malaga.com>
2023-06-17 12:56:14 +0200

    code refactored

--- includes/cub3D.h
+++ includes/cub3D.h
@@ -14,8 +14,6 @@
 # define P_SIZE		8
 # define MAP_WIDTH	1024/4
 # define MAP_HEIGHT	512/2
-# define MAP_X		8
-# define MAP_Y		8
 # define PI			3.14159265359
 # define ALPHA_INC	0.1
 # define MULT_DELTA	5
@@ -27,13 +25,6 @@ typedef struct s_player{
 	double	dx;
 	double	alpha;
 }	t_player;
-typedef struct s_rc{
-	mlx_t		*mlx;
-	mlx_image_t	*img_p;
-	mlx_image_t	*img_map;
-	mlx_image_t	*img_h;
-	t_player	player;
-}	t_rc;
 
 typedef struct s_map {
 	char			**map;
@@ -46,6 +37,17 @@ typedef struct s_map {
 	unsigned int	f_hex;
 	unsigned int	c_hex;
 }	t_map;
+
+typedef struct s_rc{
+	mlx_t		*mlx;
+	mlx_image_t	*img_p;
+	mlx_image_t	*img_map;
+	mlx_image_t	*img_h;
+	t_player	player;
+	t_map		*map;
+	int			sq_size_x;
+	int			sq_size_y;
+}	t_rc;
 
 typedef struct s_init {
 	char	*in_route;
